1da57b2cc1d869cf1beb928872b462f76adc8234
SAGAR C BELLAD <scbellad2025@gmail.com>
2020-05-15 22:29:21 +0530

    Final commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,31 @@
 //
 
 import UIKit
+import AVFoundation //Used work with audio and camera
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!  //A audio player used to play an audio data from a file memory
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton)   //sender is contains details of the button pressed
+    {
+        //print(sender.backgroundColor)
+        playSound(buttonAddress : sender.currentTitle!)
+    }
     
-
+    func playSound(buttonAddress : String) {
+        let url = Bundle.main.url(forResource: buttonAddress, withExtension: "wav")
+        //Bundle-A directory where certain resource is stored
+        //main-It is a constructor
+        //url-It is an object which helps us to access a file
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
